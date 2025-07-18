bb7844c359d241a172f3c10d15106dcaa1668677
sharnin.k <ksharnink@bk.ru>
2025-07-19 02:20:43 +0300

    fix

--- server/api/mining-data.ts
+++ server/api/mining-data.ts
@@ -1,8 +1,10 @@
 import { readFile } from "node:fs/promises";
 import { resolve } from "node:path";
 
-export default defineEventHandler(async () => {
-    console.log('Trying to read file...')
+export default defineEventHandler(async (event) => {
+    console.log("Trying to read file...");
+    setResponseHeader(event, "Access-Control-Allow-Origin", "*");
+    setResponseHeader(event, "Access-Control-Allow-Methods", "GET, POST");
     try {
         const dataPath = resolve("./server/data/miningEquipment.json");
         const fileContent = await readFile(dataPath, "utf-8");
