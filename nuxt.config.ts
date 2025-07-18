35314f882396056419fecaddd33dfc47fb848f96
sharnin.k <ksharnink@bk.ru>
2025-07-19 01:21:07 +0300

    fix: change config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
     compatibilityDate: "2025-07-15",
+    ssr: true,
     devtools: { enabled: false },
     modules: ["@nuxt/eslint", "@nuxt/icon", "@pinia/nuxt"],
     css: ["~/assets/css/main.css"],
